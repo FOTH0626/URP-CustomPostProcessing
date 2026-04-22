d9a278f394da494b230a2480ba3f706f7c6eba68
FOTH <fothli0626@gmail.com>
2026-04-22 22:29:34 +0800

    add : RadialBlur

---
+++ Assets/Scripts/Effect/RadialBlur/RadialBlur.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+public class RadialBlur : CustomPostProcessing
+{
+    private static readonly int RadialBlurParams = Shader.PropertyToID("_RadialBlurParams");
+
+    public ClampedFloatParameter BlurRadius = new ClampedFloatParameter(0f,0f,1f);
+    public ClampedIntParameter Iteration = new ClampedIntParameter (10,2,30);
+    public ClampedFloatParameter RadialCenterX = new ClampedFloatParameter (0.5f,0f,1f);
+    public ClampedFloatParameter RadialCenterY = new ClampedFloatParameter(0.5f,0f,1f);
+    
+    private Material _material;
+    private const string shaderName = "Hidden/PostProcess/RadialBlur";
+
+    public override CustomPostProcessInjectionPoint InjectionPoint => CustomPostProcessInjectionPoint.AfterPostProcess;
+    public override int OrderInInjectionPoint => 10;
+
+    public override bool IsActive()
+     => _material != null && BlurRadius.value > 0f;
+    
+
+    public override void Setup()
+    {
+        if (_material == null)
+        {
+            _material = CoreUtils.CreateEngineMaterial(shaderName);
+        }
+    }
+
+    public override void Render(CommandBuffer cmd, ref RenderingData renderingData, RTHandle source, RTHandle destination)
+    {
+        if (_material == null) return;
+        
+            var Params = new Vector4(
+                BlurRadius.value * 0.02f,
+                Iteration.value,
+                RadialCenterX.value,
+                RadialCenterY.value
+            );
+            _material.SetVector(RadialBlurParams,Params);
+            Blitter.BlitCameraTexture(cmd,source,destination,_material,0);
+        
+    }
+
+    public override void Dispose(bool disposing)
+    {
+        base.Dispose(disposing);
+        CoreUtils.Destroy(_material);
+    }
+}
---
+++ Assets/Scripts/Effect/RadialBlur/RadialBlur.shader
@@ -0,0 +1,57 @@
+﻿Shader "Hidden/PostProcess/RadialBlur"
+{
+    Properties
+    {
+        _MainTex ("Albedo (RGB)", 2D) = "white" {}
+    }
+    SubShader
+    {
+        Tags
+        {
+            "RenderType"="Opaque"
+        }
+        LOD 200
+
+
+        Pass
+        {
+            Name "RadialBlurPass"
+
+            HLSLPROGRAM
+            #include "Assets/Shader/PostProcessing.hlsl"
+
+            #pragma vertex Vert
+            #pragma fragment Frag
+
+            CBUFFER_START(UnityPerMaterial)
+                float4 _RadialBlurParams;
+            CBUFFER_END
+
+            float4 Frag(Varyings input):SV_Target
+            {
+                float2 uv = input.texcoord;
+                
+                float BlurRadius = _RadialBlurParams.x;
+                int Iteration = (int)floor(_RadialBlurParams.y);
+                float2 RadialCenter = _RadialBlurParams.zw;
+                
+                float2 blurVector = (RadialCenter - uv) * BlurRadius;
+
+                real4 acumulateColor = half4(0, 0, 0, 0);
+
+                UNITY_UNROLLX(30)
+                for (int j = 0; j < Iteration; j++)
+                {
+                    acumulateColor += SAMPLE_TEXTURE2D(_BlitTexture, sampler_PointClamp, uv);
+                    uv += blurVector;
+                }
+                
+                real4 color = acumulateColor/Iteration;
+
+                return color;
+            }
+            ENDHLSL
+        }
+
+    }
+}
