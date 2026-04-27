fe9dcd833dd64e3c5ca33f990c63ce5b6f829118
FOTH <fothli0626@gmail.com>
2026-04-27 16:27:21 +0800

    update : negative

---
+++ Assets/Scripts/Effect/Negative/Negative.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+[VolumeComponentMenu("Custom Post-processing/Negative")]
+public class Negative :CustomPostProcessing
+{
+    #region Parameters
+
+    public BoolParameter EffectActive = new BoolParameter(false);
+
+    #endregion
+
+    private Material _material;
+    private readonly string _shaderName = "Hidden/PostProcess/Negative";
+    public override CustomPostProcessInjectionPoint InjectionPoint =>
+        CustomPostProcessInjectionPoint.AfterPostProcess;
+
+    public override int OrderInInjectionPoint => 1;
+    
+    public override bool IsActive()
+    {
+        return EffectActive.value;
+    }
+
+    public override void Setup()
+    {
+        if (_material == null)
+        {
+            _material = CoreUtils.CreateEngineMaterial(_shaderName);
+        }
+    }
+
+    public override void Render(CommandBuffer cmd, ref RenderingData renderingData, RTHandle source, RTHandle destination)
+    {
+        if (_material == null) return;
+
+        Blitter.BlitCameraTexture(cmd, source, destination, _material, 0);
+    }
+}
---
+++ Assets/Scripts/Effect/Negative/Negative.shader
@@ -0,0 +1,43 @@
+﻿Shader "Hidden/PostProcess/Negative"
+{
+    Properties
+    {
+
+//        [HideInInspector]_MainTex ("Base (RGB)", 2D) = "white" {}
+
+    }
+    SubShader
+    {
+        Tags
+        {
+            "RenderType"="Opaque"
+        }
+        LOD 200
+
+        Pass
+        {
+            Name"ColorBlitPass"
+
+            HLSLPROGRAM
+            #include "Assets/Shader/PostProcessing.hlsl"
+
+
+            #pragma vertex Vert
+            #pragma fragment frag
+
+            CBUFFER_START(UnityPerMaterial)
+
+            CBUFFER_END
+
+            float4 frag(Varyings input) :SV_Target
+            {
+
+                float4 color = SAMPLE_TEXTURE2D(_BlitTexture, sampler_PointClamp, input.texcoord);
+                color = 1- color;
+                return color;
+            }
+            ENDHLSL
+        }
+
+    }
+}
